916b9a7df3b03cf96df7f91e8eb91806e776993c
Matthew Lewis <matt@mplewis.com>
2014-06-02 01:08:25 -0700

    Rotate to better angles; add some static buttons; make a function non-var'd; add view functions

--- scripts/view.js
+++ scripts/view.js
@@ -7,7 +7,9 @@ var dom = {
   },
   input: {},
   static: {
-    ticket: $('#ticket')
+    ticket: $('#ticket'),
+    printButton: $('#button-print'),
+    expiryText: $('.expiry')
   }
 };
 
@@ -19,7 +21,7 @@ var touched = {
 function populateDropdowns(stations, order) {
   var origins = $('<select>');
   order.forEach(function(stationId) {
-    var stationName = stations[stationId];
+    var stationName = stations[stationId].name;
     var option = $('<option>');
     option.val(stationId).text(stationName);
     origins.append(option);
@@ -41,13 +43,13 @@ function renderTime(time) {
   }
 }
 
-var changeHandler = function(name) {
+function changeHandler(name) {
   touched[name] = dom.input[name].val();
   if (touched.origin && touched.dest) {
     var lastDepart = getLastDepart(touched.origin, touched.dest, true);
     renderTime(lastDepart);
   }
-};
+}
 
 function bindInputs() {
   dom.input.origin.change(function() {
@@ -57,6 +59,7 @@ function bindInputs() {
     changeHandler('dest');
   });
   $('#button-print').click(function() {
+    var angle = randInt(-5, 5) + 'deg';
     dom.static.ticket
       .clearQueue()
       .transition({opacity: 0}, 750, 'easeInOutSine') // fade out existing ticket
@@ -70,6 +73,25 @@ function bindInputs() {
       .transition({y: 175}, 200, 'linear') // delay
       .transition({y: 200}, 500, 'linear') // out more
       .transition({y: 200}, 800, 'linear') // delay
-      .transition({y: 375, rotate: '-5deg'}, 500, 'easeOutExpo'); // spit it out
+      .transition({y: 375, rotate: angle}, 500, 'easeOutExpo'); // spit it out
   });
+}
+
+function setOriginById(stationId) {
+  dom.input.origin.val(stationId);
+}
+
+function setDestById(stationId) {
+  dom.input.dest.val(stationId);
+}
+
+function setExpiryText(targetDate) {
+  var formatString = 'D MMM YY h:mm A';
+  var boardText = 'BOARD ' + moment(targetDate).format(formatString).toUpperCase();
+  dom.static.expiryText.text(boardText);
+}
+
+function printTicket() {
+  setExpiryText(new Date());
+  dom.static.printButton.click();
 }
