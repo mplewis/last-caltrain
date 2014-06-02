5173b29bb4964b1e491631299e5bc2bdb9fe47d6
Matthew Lewis <matt@mplewis.com>
2014-06-02 01:07:20 -0700

    Move logic out of fetch

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,12 +1,7 @@
-var config;
+function fetchConfig(callback) {
+  $.getJSON('data/config.json', callback);
+}
 
-$('document').ready(function(){
-  $.getJSON('data/config.json', function(data) {
-    config = data;
-    $.getJSON('data/caltrain.json', function(data) {
-      populateDropdowns(data.stations, data.stationDisplayOrder);
-      initializeTimetable(data.latestDepartures);
-      bindInputs();
-    });
-  });
-});
+function fetchCaltrainData(callback) {
+  $.getJSON('data/caltrain.json', callback);
+}
